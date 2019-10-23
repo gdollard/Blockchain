5eccfee90e8a4ad77c87d26726ee43e0dc7e5584
Glenn Dollard <glenn.dollard@gmail.com>
2019-10-23 16:31:07 +0100

    updates

--- HashCheck.py
+++ HashCheck.py
@@ -12,8 +12,16 @@ def hash_this_with_date(data_to_hash, useDate=False):
     return the_hasher.hexdigest()
 
 
+num_hashes = input("Enter the number of hashes to check: ")
+try:
+    number = int(num_hashes)
+except ValueError:
+    print("Invalid number specified!")
+    # quit the program if we have invalid data
+    exit()
+
 user_data = input("Enter data to generate the Hash Value:")
 returned_hash_with_date = hash_this_with_date(user_data, True)
 returned_hash_no_date = hash_this_with_date(user_data, False)
-print("Hash for data with Date included: " + user_data + " is: " + returned_hash_with_date)
-print("Hash for data without date included: " + user_data + " is: " + returned_hash_no_date)
+print("Hash for data with Date: " + user_data + " is: " + returned_hash_with_date)
+print("Hash for data without Date: " + user_data + " is: " + returned_hash_no_date)
