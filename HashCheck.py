73ebca7682f314c07e7c1a571c30839dd76a8359
gdollard <glenn.dollard@gmail.com>
2019-10-23 17:10:26 +0100

    add hashing options and validation

--- HashCheck.py
+++ HashCheck.py
@@ -1,10 +1,10 @@
 import datetime
 import hashlib
 
+the_hasher = None
 
 def hash_this_with_date(data_to_hash, useDate=False):
     encoded_data = data_to_hash.encode('utf-8')
-    the_hasher = hashlib.sha256()
     the_hasher.update(encoded_data)
     if useDate:
         timestamp = datetime.datetime.now()
@@ -26,9 +26,34 @@ num_hashes = input("Enter the number of hashes to check: ")
 try:
     num_hashes = int(num_hashes)
 except ValueError:
-    print("Invalid number specified!")
+    print("Invalid number specified, quitting")
     # quit the program if we have invalid data
     exit()
+
+hash_mode = input("Select your choice of Hashing:\n 1. Sha1 \n 2: Sha224 \n 3: Sha256 \n 4: Sha384 \n 5: Blake2b \n 6: Blake2bs")
+try:
+    hash_mode = int(hash_mode)
+except ValueError:
+    print("Invalid Hash Mode entered, quitting.")
+    exit()
+
+if hash_mode == 1:
+    the_hasher = hashlib.sha1()
+elif hash_mode == 2:
+    the_hasher = hashlib.sha3_224()
+elif hash_mode == 3:
+    the_hasher = hashlib.sha256()
+elif hash_mode == 4:
+    the_hasher = hashlib.sha384()
+elif hash_mode == 5:
+    the_hasher = hashlib.blake2b()
+elif hash_mode == 6:
+    the_hasher = hashlib.blake2s()
+else:
+    print("Invalid hashing option selected, quitting.")
+    exit(-1)
+
+
 
 counter = 0
 while counter < num_hashes:
